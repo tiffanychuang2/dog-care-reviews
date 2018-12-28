01b898060bf95d176de83439913d1ac8e0bb7f22
Tiffany <tiffany.chuang2@gmail.com>
2018-12-28 18:30:44 -0500

    initial commit

---
+++ src/main/java/com/dogcarereviews/DogCareReviewsApplication.java
@@ -0,0 +1,14 @@
+package com.dogcarereviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogCareReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogCareReviewsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/dogcarereviews/DogCareReviewsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dogcarereviews;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DogCareReviewsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
